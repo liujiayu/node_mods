211b9b83d349e52f062e05857b6f6cc2f6182b13
Nate Lee <nate.lee@logicsolutions.com>
2015-06-11 00:18:25 +0800

    Add contents to facility page

--- src/app/system/physical/facility-controller.js
+++ src/app/system/physical/facility-controller.js
@@ -7,7 +7,47 @@
 
     FacilityCtrl.$inject = ['$scope'];
     function FacilityCtrl($scope) {
-      
+      // Scope variables
+      $scope.facilities = [
+        {
+          id: '001',
+          fullName: 'facility 001',
+          shortName: 'f001',
+          isActive: true
+        },
+        {
+          id: '810',
+          fullName: 'facility 810',
+          shortName: 'f810',
+          isActive: false
+        }
+      ];
+
+      $scope.showColumn1 = true;
+      $scope.showColumn2 = true;
+      $scope.showColumn3 = true;
+      $scope.showColumn4 = true;
+
+
+      // Scope actions
+      $scope.addFacility = addFacility;
+      $scope.displayFacility = displayFacility;
+      $scope.editFacility = editFacility;
+      $scope.deleteFacility = deleteFacility;
+
+
+      // Functions
+      function addFacility() {
+      }
+
+      function displayFacility(facility) {
+      }
+
+      function editFacility(facility) {
+      }
+
+      function deleteFacility(facility) {
+      }
     }
 
 })();
