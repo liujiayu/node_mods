41b8159d45e2e43fab4b5491de84677b3394e2ee
Nate Lee <nate.lee@logicsolutions.com>
2015-06-11 16:42:48 +0800

    Add sample table on step 2

--- src/app/system/physical/facility-edit-controller.js
+++ src/app/system/physical/facility-edit-controller.js
@@ -7,9 +7,33 @@
 
     FacilityEditCtrl.$inject = ['$scope', '$state', '$stateParams'];
     function FacilityEditCtrl($scope, $state, $stateParams) {
+      // Metronic component
       FormWizard.init();
-      
+
+
       // Scope variables
+      $scope.corporates = [
+        {
+          "id": "1101",
+          "fullName": "West Exploration",
+          "shortName": "WE",
+          "type": "Group",
+          "managerId": "Eve Yong",
+          "languageId": "",
+          "status": "Active"
+        },
+        {
+          "id": "1102",
+          "fullName": "East Exploration",
+          "shortName": "EE",
+          "type": "Region",
+          "managerId": "",
+          "languageId": "EN",
+          "status": "Active"
+        }
+      ];
+
+
       // Scope actions
       $scope.back = back;
 
@@ -18,7 +42,5 @@
       function back() {
         $state.go('facility');
       }
-
     }
-
 })();
