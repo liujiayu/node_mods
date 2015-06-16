75e4fa66c307944edaf960d1722af2f62d6350ca
Nate Lee <nate.lee@logicsolutions.com>
2015-06-16 15:43:36 +0800

    Run directive "thSorting" after the DOM has finished rendering

--- src/app/common/directives/th-sorting-directive.js
+++ src/app/common/directives/th-sorting-directive.js
@@ -5,48 +5,52 @@
     .module('qcs')
     .directive('thSorting', thSorting);
 
-    function thSorting() {
+    thSorting.$inject = ['$timeout'];
+    function thSorting($timeout) {
       return {
         restrict: 'A',
         scope: {
           ignored: '=thIgnored'
         },
         link: function(scope, elem, attr) {
-          var th = elem.find('th');
-          var ignored = scope.ignored;
+          $timeout(handlingTheadClass, 0)
 
-          th.addClass('sorting');
+          function handlingTheadClass(){
+            var th = elem.find('th');
+            var ignored = scope.ignored;
 
-          if (ignored) {
-            if (jQuery.isNumeric(ignored)) {
-              $(th[ignored]).removeClass();
+            th.addClass('sorting');
+
+            if (ignored) {
+              if (jQuery.isNumeric(ignored)) {
+                $(th[ignored]).removeClass();
+              }
+              else if (jQuery.isArray(ignored)) {
+                jQuery.each(ignored, function(i, val) {
+                  $(th[val]).removeClass();
+                });
+              }
             }
-            else if (jQuery.isArray(ignored)) {
-              jQuery.each(ignored, function(i, val) {
-                $(th[val]).removeClass();
-              });
-            }
-          }
 
-          th.click(function () {
-              var target = $(this);
+            th.click(function () {
+                var target = $(this);
+                if ( !target.hasClass('sorting_asc') && !target.hasClass('sorting_desc') ) {
+                    clearClass();
+                    target.addClass('sorting_asc');
+                } 
+                else if (target.hasClass('sorting_asc')) {
+                    clearClass();
+                    target.addClass('sorting_desc');
+                } 
+                else if (target.hasClass('sorting_desc')) {
+                    clearClass();
+                    target.addClass('sorting_asc');
+                } 
+            });
 
-              if ( !target.hasClass('sorting_asc') && !target.hasClass('sorting_desc') ) {
-                  clearClass();
-                  target.addClass('sorting_asc');
-              } 
-              else if (target.hasClass('sorting_asc')) {
-                  clearClass();
-                  target.addClass('sorting_desc');
-              } 
-              else if (target.hasClass('sorting_desc')) {
-                  clearClass();
-                  target.addClass('sorting_asc');
-              } 
-          });
-
-          function clearClass() {
+            function clearClass() {
               th.removeClass('sorting_asc sorting_desc');
+            }
           }
         }
       };
