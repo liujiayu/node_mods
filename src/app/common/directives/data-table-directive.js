b5aee35c2bac148285d53eae578da8e7904cfee1
Nate Lee <nate.lee@logicsolutions.com>
2015-06-16 17:08:45 +0800

    Add page info and clear up controller

--- src/app/common/directives/data-table-directive.js
+++ src/app/common/directives/data-table-directive.js
@@ -19,23 +19,59 @@
           actionDelete: '='
         },
         link: function(scope, elem, attr) {
+          // Local variables
+          var total, current, perPage, isLastPage;
+
+
+          // Scope variables
           scope.pagination = {
             total: scope.tableSource.length,
             perPage: 10,
             current: 1,
             maxSize: 10
           }
-
+          
           scope.orderByField = scope.tableCell[0];
           scope.orderReverse = false;
 
-          scope.orderBy = function(field) {
+
+          // Scope actions
+          scope.orderBy = orderBy;
+
+
+          // Init
+          updatePageInfo()
+
+
+          // Listener
+          scope.$watch('pagination', function(newValue, oldValue, scope) {
+            if (newValue !== oldValue) {
+              updatePageInfo();
+            }
+          }, true);
+
+
+          // Functions
+          function orderBy(field) {
             if (scope.orderByField === field) {
               scope.orderReverse = !scope.orderReverse;  
             } else {
               scope.orderReverse = false;
             }
             scope.orderByField = field;
+          }
+
+          function updatePageInfo() {
+            total = scope.pagination.total;
+            current = scope.pagination.current;
+            perPage = scope.pagination.perPage;
+            isLastPage = ((perPage * current) >= total);
+
+            scope.pageInfo = {
+              first: perPage * (current - 1) + 1,
+              last: isLastPage ? total : perPage * current,
+              total: total
+            };
           }
         }
 
